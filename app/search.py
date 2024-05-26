b6fccd08d9e1398b79c8f97a99d6735151f3c9d9
Amethyst <severus1257@gmail.com>
2024-05-26 14:57:27 +0900

    wikipediaの候補が曖昧な際のエラーを改善

--- app/search.py
+++ app/search.py
@@ -3,11 +3,20 @@ wikipedia.set_lang("ja")
 
 def get_wikipedia_text(word):
     search_list = wikipedia.search(word)
-    if search_list:
-        page = wikipedia.page(search_list[0])
-        text = page.content.split("\n\n\n== 符号位置")[0].split("\n\n\n== 脚注")[0]
-        return [search_list[0], text]
-    else:
-        return []
+    try:
+        if search_list:
+            result = get_formed_result(search_list[0])
+        else:
+            result = []
+    except wikipedia.DisambiguationError as e:
+        first_option = e.options[0]
+        try:
+            result = get_formed_result(first_option)
+        except Exception as e:
+            return f"エラーが発生しました: {e}"
+    return result
 
-print(get_wikipedia_text("ミラノ風ドリア"))
+def get_formed_result(word):
+    page = wikipedia.page(word)
+    text = page.content.split("\n\n\n== 符号位置")[0].split("\n\n\n== 脚注")[0]
+    return [word, text]
