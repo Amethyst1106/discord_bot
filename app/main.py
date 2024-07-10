68aacec83c6c6830347ecfea3532b4e6aeeb2e8b
Amethyst <severus1257@gmail.com>
2024-07-11 01:58:41 +0900

    修正の続き

--- app/main.py
+++ app/main.py
@@ -80,7 +80,7 @@ async def chat(interaction: discord.Interaction,
     chat_ai = AIs_dic[model][guild_id]
     result, embed, text_file = await chat_ai.return_answer(interaction, text, image, video)
     files = []
-    video_file = discord.File(fp=video.fp, filename=video.filename) if video else None
+    video_file = discord.File(fp=await video.read(), filename=video.filename) if video else None
     for x in [text_file, video_file]:
         if x is not None:
             files.append(x)
