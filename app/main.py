92db66f0405e99ae61714e449065bd8ae97fa8e0
Amethyst <severus1257@gmail.com>
2024-08-30 20:09:25 +0900

    バージョンを最新に変更

--- app/main.py
+++ app/main.py
@@ -34,8 +34,8 @@ default_config = genai.GenerationConfig(temperature=0.7)
 
 # Geminiモデルの設定
 nomal_model_name = "gemini-1.0-pro-latest"
-super_model_name = "gemini-1.5-pro-exp-0801"
-flash_model_name = "gemini-1.5-flash-latest"
+super_model_name = "gemini-1.5-pro-exp-0827"
+flash_model_name = "gemini-1.5-flash-exp-0827"
 
 nomal_AIs = {}
 super_AIs = {}
